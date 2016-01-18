8914944b943b1a1df567c17f48200d15f9ebbbeb
oquintansocampo <oquintansocampo@danielcastelao.org>
2016-01-18 10:58:27 +0100

    Calculadora con bucle(cierra bucle pero no lo continua)

--- src/calculadoratcpip/Servidor.java
+++ src/calculadoratcpip/Servidor.java
@@ -41,32 +41,36 @@ public class Servidor {
 
     public void read() {
         try {
+            String[] msj;
+
             System.out.println("Aceptando conexiones");
             newSocket = serverSocket.accept();
             System.out.println("Conexiónn recibida");
 
             is = newSocket.getInputStream();
             os = newSocket.getOutputStream();
-            byte[] mensaje = new byte[25];
-            is.read(mensaje);
-            String[] msj = new String(mensaje).split("-");
-            System.out.println("Op1: " + msj[0]);
-            System.out.println("Op2: " + msj[1]);
-            System.out.println("Operación: " + msj[2]);
-            int ope = Integer.parseInt(msj[2]);
-            if (ope == 1) {
-                System.out.println("Resultado: " + (Integer.valueOf(msj[0]) + Integer.valueOf(msj[1])));
-            }
-            if (ope == 2) {
-                System.out.println("Resultado: " + (Integer.valueOf(msj[0]) - Integer.valueOf(msj[1])));
-            }
-            if (ope == 3) {
-                System.out.println("Resultado: " + (Integer.valueOf(msj[0]) * Integer.valueOf(msj[1])));
-            }
-            if (ope == 4) {
-                System.out.println("Resultado: " + (Integer.valueOf(msj[0]) / Integer.valueOf(msj[1])));
-            }
-
+            do {
+                byte[] mensaje = new byte[25];
+                is.read(mensaje);
+                msj = new String(mensaje).split("-");
+                System.out.println("Op1: " + msj[0]);
+                System.out.println("Op2: " + msj[1]);
+                System.out.println("Operación: " + msj[2]);
+                int ope = Integer.parseInt(msj[2]);
+                if (ope == 1) {
+                    System.out.println("Resultado: " + (Integer.valueOf(msj[0]) + Integer.valueOf(msj[1])));
+                }
+                if (ope == 2) {
+                    System.out.println("Resultado: " + (Integer.valueOf(msj[0]) - Integer.valueOf(msj[1])));
+                }
+                if (ope == 3) {
+                    System.out.println("Resultado: " + (Integer.valueOf(msj[0]) * Integer.valueOf(msj[1])));
+                }
+                if (ope == 4) {
+                    System.out.println("Resultado: " + (Integer.valueOf(msj[0]) / Integer.valueOf(msj[1])));
+                }
+                System.out.println(msj[3]);
+            } while (msj[3].equalsIgnoreCase("n"));
         } catch (IOException ex) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
